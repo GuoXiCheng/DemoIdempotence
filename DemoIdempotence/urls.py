1e1e93c748de31dffa9ae801b3f9e3ec67d41db5
GuoXiCheng <1377994267@qq.com>
2022-03-21 21:16:43 +0800

    update

--- DemoIdempotence/urls.py
+++ DemoIdempotence/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from demo import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("demo-delete", views.DemoDelete)
 ]
