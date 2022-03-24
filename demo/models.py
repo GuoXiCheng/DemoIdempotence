81d6ef70ab2575f1b1434f15536e7d9959ec2c55
GuoXiCheng <1377994267@qq.com>
2022-03-24 21:57:32 +0800

    update

--- demo/models.py
+++ demo/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    user_id = models.CharField(db_column="user_id", max_length=255)
     user_name = models.CharField(db_column="user_name", max_length=255)
-    sex = models.CharField(db_column="sex", max_length=255)
-    age = models.IntegerField(db_column="age")
+    email = models.CharField(db_column="email", max_length=255)
 
     class Meta:
         db_table = "User"
